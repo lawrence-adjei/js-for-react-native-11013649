0b5e43dd9c55c2952d98d294d6974e095233710c
John Doe <lawdjei77@gmail.com>
2024-05-16 13:00:34 +0000

    Task 1

---
+++ arrayManipulation.js
@@ -0,0 +1,17 @@
+function processArray(arr) {
+ 
+    return arr.map(num => {
+        if (num % 2 === 0) {
+           
+            return num * num;
+        } else {
+           
+            return num * 3;
+        }
+    });
+}
+
+
+module.exports = {
+    processArray
+};
